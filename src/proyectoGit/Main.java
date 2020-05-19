5a5113c1ddac823b25d3758df3b0e8130fa7b519
Daniel Ramos Mangano <timetrolled@gmail.com>
2020-05-19 12:17:18 +0200

    Cambio

---
+++ src/proyectoGit/Main.java
@@ -0,0 +1,11 @@
+package proyectoGit;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Apéndice de método generado automáticamente
+		System.out.println("Prueba");
+		System.out.println("prueba dosdd");
+	}
+
+}
